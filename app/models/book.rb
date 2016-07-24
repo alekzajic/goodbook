0bfd5f3a12c3d1999c91f9560f390648db6a04e3
Aleksandar Zajic <alisteroz@gmail.com>
2016-07-24 16:48:26 +0200

    Added paperclip for image upload

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  # Cover image upload specifications
+  has_attached_file :cover, styles: { medium: "309x475>", thumb: "96x160>" }, default_url: "/images/:style/no-cover.png"
+  validates_attachment_content_type :cover, content_type: /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160724134851) do
+ActiveRecord::Schema.define(version: 20160724142620) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -18,8 +18,12 @@ ActiveRecord::Schema.define(version: 20160724134851) do
     t.integer  "pages"
     t.string   "year_published"
     t.string   "isbn"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "cover_file_name"
+    t.string   "cover_content_type"
+    t.integer  "cover_file_size"
+    t.datetime "cover_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
