e18c7ea4645b61b476cf86c30761205427c5e248
Aleksandar Zajic <alisteroz@gmail.com>
2016-07-24 16:13:50 +0200

    Add Books scaffold and Users

---
+++ db/migrate/20160724130852_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.text :description
+      t.integer :pages
+      t.string :year_published
+      t.string :isbn
+
+      t.timestamps
+    end
+  end
+end
